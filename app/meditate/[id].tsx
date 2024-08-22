6482eda0c8ec65f48a67b7e3c0d1da5a53c08b42
RahymCharyyev <r569837@gmail.com>
2024-08-22 22:03:25 +0500

    ✨feature: Added timer

---
+++ app/meditate/[id].tsx
@@ -0,0 +1,74 @@
+import AppGradient from '@/components/AppGradient';
+import meditationImages from '@/constants/meditation-images';
+import { router, useLocalSearchParams } from 'expo-router';
+import React, { useEffect, useState } from 'react';
+import { ImageBackground, Pressable, Text, View } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import CustomButton from '@/components/CustomButton';
+
+const Meditate = () => {
+  const { id } = useLocalSearchParams();
+  const [secondsRemaining, setSecondsRemaining] = useState(10);
+  const [isMeditating, setMeditating] = useState(false);
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout;
+
+    if (secondsRemaining === 0) {
+      setMeditating(false);
+      return;
+    }
+
+    if (isMeditating) {
+      timerId = setTimeout(
+        () => setSecondsRemaining(secondsRemaining - 1),
+        1000
+      );
+    }
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [secondsRemaining, isMeditating]);
+
+  const formattedTimeMinutes = String(
+    Math.floor(secondsRemaining / 60)
+  ).padStart(2, '0');
+
+  const formattedTimeSeconds = String(secondsRemaining % 60).padStart(2, '0');
+
+  return (
+    <View className='flex-1'>
+      <ImageBackground
+        source={meditationImages[Number(id) - 1]}
+        resizeMode='cover'
+        className='flex-1'
+      >
+        <AppGradient colors={['transparent', 'rgba(0, 0, 0, 0.8)']}>
+          <Pressable
+            onPress={() => router.back()}
+            className='absolute top-16 left-6 z-10'
+          >
+            <AntDesign name='leftcircleo' size={50} color='white' />
+          </Pressable>
+          <View className='flex-1 justify-center'>
+            <View className='mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center'>
+              <Text className='text-4xl text-blue-800 font-rmono'>
+                {formattedTimeMinutes}:{formattedTimeSeconds}
+              </Text>
+            </View>
+          </View>
+
+          <View className='mb-5'>
+            <CustomButton
+              title='Start meditation'
+              onPress={() => setMeditating(true)}
+            />
+          </View>
+        </AppGradient>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default Meditate;
